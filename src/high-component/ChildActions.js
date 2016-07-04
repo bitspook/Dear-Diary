d36b03f5aaf68365b6ff84dfdfc014c3e89eba3e
Charanjit Singh <ckhabra@gmail.com>
2016-07-04 09:28:46 +0530

    Fix the bug mentioned in last commit
    It is a quick fix though. Intercepting Obsrevables this way is not very
    performant, but this will have to do for now. I am already a lot behind
    my schedule :-|

--- src/high-component/ChildActions.js
+++ src/high-component/ChildActions.js
@@ -4,16 +4,23 @@ export default (...childActions) => (ChildComponent) => class ChldActionsWrapper
     constructor (props, context) {
         super(props, context);
 
-        console.warn('Got props', props);
-
         if (typeof ChildComponent.handleChildActions !== 'function') {
             console.warn(ChildComponent.name, 'component must provide a static `handleChildActions` function'); // eslint-disable-line no-console
 
             return;
         }
 
+        this.startInterceptChildActions(props);
+    }
+
+    componentWillReceiveProps = (nextProps) => {
+        this.stopInterceptChildActions();
+        this.startInterceptChildActions(nextProps || this.props);
+    }
+
+    startInterceptChildActions = (props) => {
         const ChildActions = ChildComponent
-            .handleChildActions(this.props, ...childActions)
+            .handleChildActions(props, ...childActions)
             .catch((err) => {
                 console.error('Error occurred while handling child actions of ', ChildComponent.name, ':', err); // eslint-disable-line no-console
 
@@ -21,12 +28,16 @@ export default (...childActions) => (ChildComponent) => class ChldActionsWrapper
             });
 
         this.childActionsSub = ChildActions.subscribe();
-    }
+    };
 
-    componentWillUnmount = () => {
+    stopInterceptChildActions = () => {
         if (this.childActionsSub) {
             this.childActionsSub.unsubscribe();
         }
+    };
+
+    componentWillUnmount = () => {
+        this.stopInterceptChildActions();
     }
 
     render () {
