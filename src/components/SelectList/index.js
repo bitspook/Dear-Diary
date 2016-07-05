3626161c723b2d3c5f2abc9c8332d8918653e75e
Charanjit Singh <ckhabra@gmail.com>
2016-07-05 07:20:01 +0530

    Make SelectList filter work with internal state

--- src/components/SelectList/index.js
+++ src/components/SelectList/index.js
@@ -1,12 +1,57 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
+import {Link} from 'react-router';
+import './style.scss';
 
 class SelectList extends Component {
+    static propTypes = {
+        items: PropTypes.arrayOf(PropTypes.any),
+        title: PropTypes.string
+    };
+
+    constructor (props, context) {
+        super(props, context);
+
+        this.state = {items: props.items};
+    }
+
+    handleFilterTags = (event) => {
+        const filter = event.target.value;
+        const items = this.props.items.filter((item) => item.indexOf(filter) >= 0);
+
+        this.setState({items});
+    };
+
     render () {
-        return <div className='SelectList__wrapper'>
-            <ul></ul>
+        const items = this.state.items;
+
+        return <div className='SelectList__container'>
+            <ul className='SelectList__list'>
+                <li>
+                    <input
+                        className='SelectList__search-input'
+                        onChange={this.handleFilterTags}
+                        placeholder='Search'
+                        tyle='search'
+                    />
+                </li>
+
+                {/* eslint-disable prefer-arrow-callback */}
+                {items.sort().map(function (item) {
+                    return (
+                        <Link
+                            activeClassName='SelectList__list-item-active'
+                            key={item}
+                            query={{tag: item}}
+                            to='/entries'
+                        >
+                            <li className='SelectList__list-item'>{item}</li>
+                        </Link>);
+                })}
+                {/* eslint-enable */}
+            </ul>
         </div>;
     }
-};
+}
 
 export {
     SelectList
